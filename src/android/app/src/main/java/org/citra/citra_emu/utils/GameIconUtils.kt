7f2fede3f90d21fc24a20aceadaa96c6875a2867
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-03-30 13:45:32 +0530

    use the old method but use ``ARGB_8888``

--- src/android/app/src/main/java/org/citra/citra_emu/utils/GameIconUtils.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/utils/GameIconUtils.kt
@@ -6,8 +6,6 @@ package org.citra.citra_emu.utils
 
 import android.graphics.Bitmap
 import android.widget.ImageView
-import android.graphics.Canvas
-import android.graphics.Matrix
 import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.FragmentActivity
 import coil.ImageLoader
@@ -37,32 +35,10 @@ class GameIconFetcher(
     }
 
     private fun getGameIcon(vector: IntArray?): Bitmap? {
-        // Check for null or empty vector
-        if (vector == null || vector.isEmpty()) return null
-
-        try {
-            // Assuming the input vector represents a square image, calculate the dimension
-            val dimension = Math.sqrt(vector.size.toDouble()).toInt()
-        
-            // Create a temporary bitmap with the original dimension for better quality
-            val tempBitmap = Bitmap.createBitmap(dimension, dimension, Bitmap.Config.ARGB_8888)
-            tempBitmap.copyPixelsFromBuffer(IntBuffer.wrap(vector))
-
-            // Create the final bitmap with desired dimensions and ARGB_8888 for better quality
-            val finalBitmap = Bitmap.createBitmap(48, 48, Bitmap.Config.ARGB_8888)
-        
-            // Scale the tempBitmap to fit into the finalBitmap
-            val canvas = Canvas(finalBitmap)
-            val scale = 48f / dimension
-            val matrix = Matrix().apply { postScale(scale, scale) }
-            canvas.drawBitmap(tempBitmap, matrix, null)
-
-            return finalBitmap
-         } catch (e: Exception) {
-             e.printStackTrace()
-             return null
-         }
-     }
+        val bitmap = Bitmap.createBitmap(48, 48, Bitmap.Config.ARGB_8888)
+        bitmap.copyPixelsFromBuffer(IntBuffer.wrap(vector))
+        return bitmap
+    }
 
     class Factory : Fetcher.Factory<Game> {
         override fun create(data: Game, options: Options, imageLoader: ImageLoader): Fetcher =
